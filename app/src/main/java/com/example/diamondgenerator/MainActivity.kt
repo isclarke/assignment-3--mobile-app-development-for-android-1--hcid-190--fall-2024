0d6d913e408850a65d8fce746e088f611db17de0
isclarke <156957402+isclarke@users.noreply.github.com>
2024-12-02 14:28:52 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/diamondgenerator/MainActivity.kt
+++ app/src/main/java/com/example/diamondgenerator/MainActivity.kt
@@ -4,44 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diamondgenerator.ui.theme.DiamondGeneratorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {fwfw
-            DiamondGeneratorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContent {
+          DiamondGeneratorTheme {
+            Surface(
+              modifier = Modifier.fillMaxSize(),
+              color = MaterialTheme.colorScheme.background
+            ) {
+              Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+              ) {
+                Column(
+                  modifier = Modifier
+                    .fillMaxWidth(),
+                  verticalArrangment = Arrangement.Center,
+                  horiziontalAlignment = Alignment.CenterHorizontally
+                ) {
+                  GreetingText(meesage = "Diamond Generator")
+                  Spacer(modifier = Modifier.height(16.dp))
                 }
+              }
             }
+          }
+
+
+          @Composable
+          fun Greeting(name: String, modifier: Modifier = Modifier) {
+            Text(
+              text = "Diamond amount",
+              modifier = modifier
+            )
+          }
+
+          @Preview(showBackground = true)
+          @Composable
+          fun GreetingPreview() {
+            DiamondGeneratorTheme {
+              Greeting("Android")
+            }
+          }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiamondGeneratorTheme {
-        Greeting("Android")
-    }
-}
